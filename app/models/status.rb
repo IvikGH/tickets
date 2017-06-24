a543eacccdaad2ae16d20283ab8259a6b102a6a2
Igor <andoidxx@gmail.com>
2017-06-24 23:08:07 +0300

    Implement tickets#index

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,6 @@
 class Status < ApplicationRecord
+
+  has_many :tickets
+
+  CLOSED_STATUSES = %w[Cancelled Completed]
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
 
   belongs_to :status
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :department
+
 end
