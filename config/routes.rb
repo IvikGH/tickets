dd68f151adf84784b6eae060c2d31cc137504a6b
Igor <andoidxx@gmail.com>
2017-06-29 18:04:08 +0300

    Fix heroku sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
   end
   resources :departments
   resources :statuses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
